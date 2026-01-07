02c48507c70efadea608b1fcd896a46cf5a9af9f
subanu03 <smounttech97@gmail.com>
2026-01-07 11:23:14 +0530

    test auto update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins + Docker + GitHub is sucessfully 🐼fdgfdg!"
+    return "Hello everyone  from Jenkins + Docker + GitHub is sucessfully 🐼fdgfdg!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
